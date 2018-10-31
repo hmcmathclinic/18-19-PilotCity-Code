b3a598d63f0a1dd0a52b0d9535c5ce83eacfdb9c
Aanya Alwani <aalwani@febrius.Math.HMC.Edu>
2018-10-30 20:25:43 -0700

    ...

---
+++ webapp/backend/user_dao.py
@@ -0,0 +1,18 @@
+import abc
+
+class UserDao(abc.ABC):
+
+
+    @abc.abstractmethod
+    def fetch_user_data(self, user_id):
+        pass
+
+
+    @abc.abstractmethod
+    def create_user(self,user_id, new_values):
+        pass
+
+
+    @abc.abstractmethod
+    def update_user_properties(self, user_id, new_values):
+        pass
