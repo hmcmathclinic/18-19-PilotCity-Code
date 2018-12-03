c31d7f149881668454a25f6d07ca03e36fc592ea
Jean <selasijean@gmail.com>
2018-12-02 22:13:51 -0800

    bug fixes

--- webapp/backend/matchmaking/parser.py
+++ webapp/backend/matchmaking/parser.py
@@ -4,16 +4,17 @@ def getAllUserArrays():
 	users = push.get_all_users()
 	uids = users.keys()
 	for uid in uids:
-		if isEmployer(uid):
-			User_dict, employer_dict = getEmployerDictFromArray(arr, uid)
+		if isEmployer(users[uid]['form']):
+			User_dict, employer_dict = getEmployerDictFromArray(users[uid]['form'], uid)
 			push.put_data_in_users(uid, User_dict) 
 			push.put_data_in_employers(uid, employer_dict) 
-		elif isTeacher(uid):
-			User_dict, teacher_dict = getTeacherDictFromArray(arr, uid)
+		elif isTeacher(users[uid]['form']):
+			User_dict, teacher_dict = getTeacherDictFromArray(users[uid]['form'], uid)
 			push.put_data_in_users(uid, User_dict) 
 			push.put_data_in_teachers(uid, teacher_dict) 
 
 def isEmployer(arr):
+	print(arr)
 	if "employer_story" in arr[0].keys():
 		return True
 	return False
@@ -27,13 +28,13 @@ def getEmployerDictFromArray(arr, uid):
 	Users_dict = {}
 	employer_dict = {}
 
-	employer_story = arr[0]
-	employer_address = arr[1]
-	employer_sector = arr[2]
-	employer_industry = arr[3]
-	employer_solution_type = arr[4]
-	employer_solution_keywords = arr[5]
-	employer_question_keywords = arr[6]
+	employer_story = arr[0]["employer_story"]
+	employer_address = arr[1]["employer_address"]
+	employer_sector = arr[2]["employer_sector"]
+	employer_industry = arr[3]["employer_industry"]
+	employer_solution_type = arr[4]["employer_solution_type"]
+	employer_solution_keywords = arr[5]["employer_solution_keywords"]
+	employer_question_keywords = arr[6]["employer_question_keywords"]
 
 	Users_dict["email"] = push.get_user_record(uid)
 	Users_dict["first_name"] = employer_story["first_name"]
@@ -51,18 +52,19 @@ def getEmployerDictFromArray(arr, uid):
 	employer_dict["position_name"] = employer_story["position_name"]
 	employer_dict["sector"] = employer_sector["sector"]
 
-	return Users_dict, employers_dict
+	return Users_dict, employer_dict
 
 def getTeacherDictFromArray(arr, uid):
 	Users_dict = {}
 	teacher_dict = {}
 
-	teacher_story = arr[0]
-	teacher_address = arr[1]
-	teacher_class = arr[2]
-	teacher_ptype = arr[3]
-	teacher_skills = arr[4]
-	teacher_industry = arr[5]
+	teacher_story = arr[0]["teacher_story"]
+	print(teacher_story)
+	teacher_address = arr[1]["teacher_address"]
+	teacher_class = arr[2]["teacher_class"]
+	teacher_ptype = arr[3]["teacher_ptype"]
+	teacher_skills = arr[4]["teacher_skills"]
+	teacher_industry = arr[5]["teacher_industry"]
 
 	Users_dict["email"] = push.get_user_record(uid)
 	Users_dict["first_name"] = teacher_story["first_name"]
@@ -87,7 +89,7 @@ def getTeacherDictFromArray(arr, uid):
 		classroom["students"] = bad_class["Students"]
 		# Follow up and change this
 		classroom["uid"] = uid
-		classes = classes.append(classroom)
+		classes.append(classroom)
 	teacher_dict["classes"] = classes
 	teacher_dict["room_number"] = teacher_address.pop("room")
 	teacher_dict["school_addresss"] = teacher_address
