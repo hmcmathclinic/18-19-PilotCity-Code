576bff8e8fc55017b6f886554d58ca3e52847eea
Jean <selasijean@gmail.com>
2018-12-02 21:38:52 -0800

    comm with firebase admin

---
+++ webapp/backend/push.py
@@ -0,0 +1,42 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+
+
+cred = credentials.Certificate('service_account.json')
+firebase_admin.initialize_app(cred, options={
+    'databaseURL': 'https://my-project-id.firebaseio.com'
+})
+db = firestore.client()
+
+
+def get_user_record(uid):
+    user = auth.get_user(uid)
+    return user.email
+
+
+def get_all_users():
+    out = []
+    users = db.collection("users").get()
+    for user in users:
+        user_dict = {}
+        user_dict[user.id] = user.to_dict()
+        out.append(user_dict)
+    return out
+
+
+def put_data_in_users(user_id, value):
+    db.collection("Users").document(user_id).set(value)
+
+
+def put_data_in_teachers(user_id, value):
+    db.collection("teachers").document(user_id).set(value)
+
+
+def put_data_in_employers(user_id, value):
+    db.collection("employers").document(user_id).set(value)
+
+
+if __name__ == "__main__":
+    get_user_record("49Z7lfsLuihpCaJUZBpuZ0g2rGt1")
