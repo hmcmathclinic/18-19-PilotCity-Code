463be48dc6fd23cdd64fd855b1e6351122774ab4
Jean <selasijean@gmail.com>
2018-12-11 19:37:10 -0800

    multithreading to possibly improve runtime

--- webapp/backend/getRankedTeachers_old.py
+++ webapp/backend/getRankedTeachers_old.py
@@ -3,12 +3,14 @@ import sys
 from user_dao_impl import UserDaoImpl
 import time
 import utilities
+import concurrent
 
 
 class RankingTeachers: #emplyer scoring the teachers
 
 
     def __init__(self, employer_id, user_dao, utilities):
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
         self.dao = user_dao
         self.all_teachers = self.dao.fetch_all_teachers()
         self.teacher_ids = list(self.all_teachers.keys())
@@ -20,6 +22,8 @@ class RankingTeachers: #emplyer scoring the teachers
     def getRankedList(self):
         teacher_dict = {}
         teacher_list = []
+        future_to_teacher_id = {}
+        
         for teacher in self.teacher_ids:
             teacher_data = self.all_teachers[teacher]
             match = matching_old.Matching(self.employer_data, teacher_data, self.utilities)
@@ -32,8 +36,16 @@ class RankingTeachers: #emplyer scoring the teachers
                 not "selected_industry_keywords" in match.teacher_data or \
                 not "selected_skills_keywords" in match.teacher_data:
                 continue    # move onto next teacher (this one doesn't have enough info)
-            scoreT = match.score_teacher()
-            teacher_dict[teacher] = scoreT
+            future_to_teacher_id[self.executor.submit(match.score_teacher)] = teacher
+
+        for future in concurrent.futures.as_completed(future_to_teacher_id):
+            teacher_id = future_to_teacher_id[future]
+            try:
+                scoreT = future.result()
+                teacher_dict[teacher_id] = scoreT
+            except Exception as exc:
+                print('%r generated an exception: %s' % (teacher_id, exc))
+
         for key, value in sorted(teacher_dict.items(), key= lambda x: x[1], reverse=True):
             teacher_list.append(key)  
             print(str(key) + ": " + str(value))
@@ -45,7 +57,10 @@ def main():
     user_dao = UserDaoImpl()
     utils = utilities.Utils()
     rank = RankingTeachers(employer_id, user_dao, utils)
+    start = time.time()
     print("The list of ranked teachers is ", rank.getRankedList())
+    end = time.time()
+    print("Runtime for getting rankedlist: {} ".format(end - start))
 
 if __name__ == '__main__':
     main()
