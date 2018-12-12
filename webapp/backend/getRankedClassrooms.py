ff10f69295066c9cb62a0a326aa4bc8a718949ba
MadHobbs <mhobbs0760@scrippscollege.edu>
2018-12-11 16:06:02 -0800

    I think classrooms is good to go

--- webapp/backend/getRankedClassrooms.py
+++ webapp/backend/getRankedClassrooms.py
@@ -4,16 +4,23 @@ from user_dao_impl import UserDaoImpl
 
 class RankingClassrooms: #emplyer scoring the classrooms
 
-    def __init__(self):
-        self.dao = UserDaoImpl()
-        self.classroom_ids = self.dao.fetch_all_classrooms()
-        self.employer_id = self.dao.fetch_all_employers()[0]
+    def __init__(self, employer_id, user_dao, utilities):
+        self.dao = user_dao
+        self.all_classrooms = self.dao.fetch_all_classroom() # used to be called classroom_ids
+        self.all_teachers = self.dao.fetch_all_teachers()
+        self.classroom_ids = list(self.all_classrooms.keys())
+        self.employer_data = self.dao.fetch_employer_data(employer_id)
+        self.utilities = utilities
+        self.employer_id = employer_id
 
     def getRankedList(self):
         classroom_dict = {}
         classroom_list = []
         for classroom in self.classroom_ids:
-            match = matching.Matching(self.employer_id, classroom)
+            classroom_data = self.all_classrooms[classroom]
+            teacher_id = classroom_data["teacher_uid"]
+            teacher_data = self.all_teachers[teacher_id]
+            match = matching.Matching(self.employer_data, teacher_data, classroom_data, self.utilities)
             if not "selected_industry_keywords" in match.employer_data or \
                 not "selected_product_keywords" in match.employer_data or \
                 not "selected_service_keywords" in match.employer_data or \
@@ -31,7 +38,10 @@ class RankingClassrooms: #emplyer scoring the classrooms
         return classroom_list
 
 def main():
-    rank = RankingClassrooms()
+    employer_id = "7bNr6B30iscz7hL4zAvSqiN1g0l2"
+    user_dao = UserDaoImpl()
+    utils = utilities.Utils()
+    rank = RankingTeachers(employer_id, user_dao, utils)
     print("The list of ranked classrooms is ", rank.getRankedList())
 
 if __name__ == '__main__':
