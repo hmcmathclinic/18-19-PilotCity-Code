7d5076ba51245f0defd1f12ed5b313613eb60645
Jean <selasijean@gmail.com>
2019-02-07 11:35:14 -0800

    Initial commit for mobile app

---
+++ mobile/PilotCityApp/PilotCityApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PilotCityApp
+//
+//  Created by Selasi Jean Kwame Adedze on 2/7/19.
+//  Copyright © 2019 PilotCity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
