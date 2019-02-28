b3dfd68eb231889f74f2b00a4afbb5d8735455e5
Jean <selasijean@gmail.com>
2019-02-28 12:42:46 -0800

    made syllabus parsing class and added requirements.txt to simplify pip installs

--- syllabus_parsing/package/syllabiParser.py
+++ syllabus_parsing/package/syllabiParser.py
@@ -1,14 +1,71 @@
+from glob import glob
+import os
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice
+from string import punctuation
 
 class SyllabiPDFTextExtractor:
-    
-
-    def __init__(self, folder_path):
-        self.folder_path = folder_path
 
 
-    def set_folder_path(self, folder_path):
-        self.folder_path = folder_path
+    def __init__(self):
+        pass
+
+
+    def find_pdfs(self, folder_path):
+        return glob(os.path.join('{}'.format(folder_path),"*.{}".format('pdf')))
     
+
+    def convert_pdf_to_text(self, fname, pages=None):
+        if not pages:
+            pagenums = set()
+        else:
+            pagenums = set(pages)
+
+        output = StringIO()
+        manager = PDFResourceManager()
+        converter = TextConverter(manager, output, laparams=LAParams())
+        interpreter = PDFPageInterpreter(manager, converter)
+
+        infile = open(fname, 'rb')
+        
+        try:
+            for page in PDFPage.get_pages(infile, pagenums):
+                interpreter.process_page(page)
+        except PDFTextExtractionNotAllowed:
+            print('This pdf won\'t allow text extraction!')
+        infile.close()
+        converter.close()
+        text = output.getvalue()
+        output.close
+        return self.__strip_punctuation(text)
+
     
-    def get_folder_path(self):
-        return self.folder_path
+    def build_corpus_from_pdf_folder_path(self, folder_path):
+        corpus = []
+        list_of_pdfs = self.find_pdfs(folder_path)
+        for pdf in list_of_pdfs:
+            syllabus_string = self.convert_pdf_to_text(pdf)
+            corpus.append(syllabus_string)
+        return corpus
+
+    
+    def __strip_punctuation(self, s):
+        return ''.join(c for c in s if c not in punctuation)
+
+
+## Test
+# if __name__ == "__main__":
+#     parser = SyllabiPDFTextExtractor()
+#     print(parser.convert_pdf_to_text('../AllSyllabiParser/math2010.pdf'))
+#     print(parser.find_pdfs('../AllSyllabiParser'))
+     
