fc4df23aa7e55ac3707d72b310ff4910703815ee
MadHobbs <mhobbs0760@scrippscollege.edu>
2019-03-07 11:47:05 -0800

    changed this to have las positas

--- syllabus_parsing/package/learningAgents.py
+++ syllabus_parsing/package/learningAgents.py
@@ -6,6 +6,9 @@ from pdftextExtractor import PDFTextExtractor
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.decomposition import NMF
 from sklearn.preprocessing import normalize
+from glob import glob
+import os
+import pickle
 
 class LdaAgent(Model):
 
@@ -77,6 +80,19 @@ class HdaAgent(Model):
 
 if __name__ == "__main__":
     parser = PDFTextExtractor()
-    documents = parser.get_documents_from_pdf_folder_path('../AllSyllabiParser')
-    lda_agent = LdaAgent(documents)
-    print(lda_agent.train(20))
+    #documents = parser.get_documents_from_pdf_folder_path('../AllSyllabiParser')
+    
+    # las positas syllabi
+    documents = parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi')
+    documents += parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi2')
+    documents += parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi3')
+    documents += parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi4')
+    agent = NmfAgent(documents)
+    topics = agent.train(50)
+    print(topics)
+    name = "50topics_nmf_laspositas.sav"
+    with open(name, 'wb') as filehandle:
+        pickle.dump(topics, filehandle)
+    name = "nmf_model_50topics_laspositas.sav"
+    with open(name, 'wb') as filehandle:
+        pickle.dump(agent, filehandle)
