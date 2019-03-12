0c0177072c986f68c6a705a1de4999dd6d2f88ad
aalwani <Aanya@WL-201-1.WPA.Claremont.Edu>
2019-03-11 19:47:19 -0700

    Cleaned studyguide code

--- syllabus_parsing/wikipedia_parser.py
+++ syllabus_parsing/wikipedia_parser.py
@@ -9,25 +9,19 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
- 
-doc = SimpleDocTemplate("study_guide.pdf",pagesize=letter,
-                        rightMargin=72,leftMargin=72,
-                        topMargin=72,bottomMargin=18)
-Story=[]
-logo = "logo.png"
-  
-im = Image(logo, 2*inch, 2*inch)
-Story.append(im)
- 
-styles=getSampleStyleSheet()
-styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-
-# Title
-ptext = '<b><font size=25>Study Guide</font></b>'
-Story.append(Paragraph(ptext, styles["Normal"]))         
-Story.append(Spacer(1, 12))
-
 warnings.filterwarnings("ignore")
+
+
+def create_layout(logo, story, styles):  
+	im = Image(logo, 2*inch, 2*inch)
+	Story.append(im)
+	styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+	# Title
+	ptext = '<b><font size=25>Study Guide</font></b>'
+	Story.append(Paragraph(ptext, styles["Normal"]))         
+	Story.append(Spacer(1, 20))
+
 
 def get_topics():
 	index_dict = "package/20topics_nmf_laspositas.sav"
@@ -46,22 +40,32 @@ def get_definition(topic):
 		return -1
 	return summary
 
+def add_data_to_document(Story, doc, styles):
+	topics = get_topics()
+	for topic in topics:
+		s = ''
+		n = 0
+		for word in topic:
+			summary = get_definition(word)
+			if summary != -1:
+				n += 1
+				s = "<b>" + word + "</b>" + ": " + summary
+				Story.append(Spacer(1, 6))
+				Story.append(Paragraph(s, styles["Justify"]))
+			if n == 3:
+				break
+		Story.append(Spacer(1, 12))
+
+	doc.build(Story)
 
 
 
-topics = get_topics()
-for topic in topics:
-	s = ''
-	n = 0
-	for word in topic:
-		summary = get_definition(word)
-		if summary != -1:
-			n += 1
-			s = "<b>" + word + "</b>" + ": " + summary
-			Story.append(Spacer(1, 6))
-			Story.append(Paragraph(s, styles["Justify"]))
-		if n == 3:
-			break
-	Story.append(Spacer(1, 12))
+doc = SimpleDocTemplate("study_guide.pdf",pagesize=letter,
+	                        rightMargin=72,leftMargin=72,
+	                        topMargin=72,bottomMargin=18)
+Story=[]
+logo = "logo.png"
+styles=getSampleStyleSheet()
 
-doc.build(Story)
+create_layout(logo,Story,styles)
+add_data_to_document(Story, doc,styles)
