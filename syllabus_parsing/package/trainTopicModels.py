62dec6f7e3d2ba2e91046a931e06b5dd289b2ea4
MadHobbs <mhobbs0760@scrippscollege.edu>
2019-03-13 18:37:33 -0700

    some file reorg

--- syllabus_parsing/package/trainTopicModels.py
+++ syllabus_parsing/package/trainTopicModels.py
@@ -1,4 +1,34 @@
 import learningAgents
+from model import Model
+from documentCleaner import DocumentCleaner
+import gensim
+import pandas as pd
+from pdftextExtractor import PDFTextExtractor
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.decomposition import NMF
+from sklearn.preprocessing import normalize
+import pickle
+
+def trainModel(type, documents, use_tfidf=True):
+    ''' Type is either LDA or NMF. Set tfidf to true or false (LDA only) '''
+    if type == "LDA":
+        agent = learningAgents.LdaAgent(topn=100, documents=documents)
+    elif type == "NMF": 
+        agent = learningAgents.NmfAgent(topn=100, documents=documents)
+    else: 
+        return "Error! type needs to be either LDA or NMF"
+    if use_tfidf: 
+        addin = "TFIDF"
+    else:
+        addin = ""
+    for num_topics in range(1, 41, 1):
+        print("Training " + type + " model on ", str(num_topics), " topics with" + addin)
+        topics = agent.train(num_topics, use_tfidf)
+        print(topics)
+        name = str(num_topics) + "topics_" + type + addin + "_laspositas.sav"
+        agent.save_info(topics, name)
+        name = str(num_topics) + "topics_agent_" + type + addin + "_laspositas.sav"
+        agent.save_info(agent, name)
 
 if __name__ == "__main__":
     parser = PDFTextExtractor()
@@ -9,12 +39,5 @@ if __name__ == "__main__":
     documents += parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi2')
     documents += parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi3')
     documents += parser.get_documents_from_pdf_folder_path('../LosPositasSyllabi4')
-    agent = NmfAgent(documents)
-    topics = agent.train(50)
-    print(topics)
-    name = "50topics_nmf_laspositas.sav"
-    with open(name, 'wb') as filehandle:
-        pickle.dump(topics, filehandle)
-    name = "nmf_model_50topics_laspositas.sav"
-    with open(name, 'wb') as filehandle:
-        pickle.dump(agent, filehandle)
+
+    trainModel(documents=documents, use_tfidf=True, type = "NMF")
--- syllabus_parsing/selectNumTopics.py
+++ syllabus_parsing/selectNumTopics.py
@@ -4,12 +4,13 @@ from itertools import combinations
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import colors
 plt.style.use("ggplot")
 matplotlib.rcParams.update({"font.size": 14})
 
 utils = utilities.Utils()
 
-def calculate_coherence(topics_df, top = 5):
+def calculate_coherence(topics_df, top = 3):
     skipped_over = 0
     num_topics = topics_df.shape[1]
     print(num_topics)
@@ -35,34 +36,52 @@ def calculate_coherence(topics_df, top = 5):
 
 def main():
     utils = utilities.Utils()
-    #utils.score(w1.lower(), w2.lower())
 
-    topics_fname = "topics/20topics_nmf_laspositas.sav"
-    with open(topics_fname, 'rb') as filehandle:
-        topics20_df = pickle.load(filehandle)
+    nmf_topics_scoreL = []
+    nmfTFIDF_topics_scoreL = []
+    k_values = range(5, 95, 5)
+    for num_topics in k_values:
+        topics_fname = "topics/NMF_5_95_5/"+str(num_topics)+"topics_NMFTFIDF_laspositas.sav"
+        with open(topics_fname, 'rb') as filehandle:
+            topics = pickle.load(filehandle)
+        score, skipped_over = calculate_coherence(topics, top=3)
+        print(score)
+        print(skipped_over)
+        nmf_topics_scoreL.append(score)
+        topics_fname = "topics/LDA_5_95_5/"+str(num_topics)+"topics_LDATFIDF_laspositas.sav"
+        with open(topics_fname, 'rb') as filehandle:
+            topics = pickle.load(filehandle)
+        score, skipped_over = calculate_coherence(topics, top=3)
+        nmfTFIDF_topics_scoreL.append(score)
+        print("\n")
 
-    topics_fname = "topics/50topics_nmf_laspositas.sav"
-    with open(topics_fname, 'rb') as filehandle:
-        topics50_df = pickle.load(filehandle)
+    fig = plt.figure(figsize = (35,7))
+    # create the line plot
+    ax = plt.plot(k_values, nmf_topics_scoreL)
+    plt.xticks(k_values)
+    plt.xlabel("Number of Topics")
+    plt.xlabel("Mean Coherence")
+    plt.title("LDA vs. NMF, both with TFIDF")
+    # add the points
+    plt.scatter(k_values, nmf_topics_scoreL, c="purple", label = "NMF")
+    plt.scatter(k_values, nmfTFIDF_topics_scoreL,c="mediumseagreen", label = "LDA")
+    # find and annotate the maximum point on the plot
+    ymax = max(nmf_topics_scoreL)
+    xpos = nmf_topics_scoreL.index(ymax)
+    best_kNMF = k_values[xpos]
+    plt.annotate("k=%d" % best_kNMF, xy = (best_kNMF, ymax),
+                 xytext = (best_kNMF, ymax), 
+                 textcoords = "offset points", 
+                 fontsize = 16)
+    ymax = max(nmfTFIDF_topics_scoreL)
+    xpos = nmfTFIDF_topics_scoreL.index(ymax)
+    best_knmfTFIDF = k_values[xpos]
+    plt.annotate("k=%d" % best_knmfTFIDF, xy = (best_knmfTFIDF, ymax),
+                 xytext = (best_knmfTFIDF, ymax), 
+                 textcoords = "offset points", 
+                 fontsize = 16)
+    # show the plot
+    plt.show()
 
-    topics_fname = "topics/100topics_nmf_laspositas.sav"
-    with open(topics_fname, 'rb') as filehandle:
-        topics100_df = pickle.load(filehandle)
-
-    topics_fname = "topics/200topics_nmf_laspositas.sav"
-    with open(topics_fname, 'rb') as filehandle:
-        topics200_df = pickle.load(filehandle)
-
-    topic_options = [topics20_df, topics50_df, topics100_df, topics200_df]
-
-    scores = []
-    skipped = []
-    for topics in topic_options:
-        score, skipped_over = calculate_coherence(topics, top=5)
-        scores.append(score)
-        skipped.append(skipped_over)
-    print(scores)
-    print(skipped)
-
-
+main()
 
