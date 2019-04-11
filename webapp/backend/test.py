876697b09b23d1963883a01d6363a45a9dca7235
aalwani <Aanya@WL-201-1.WPA.Claremont.Edu>
2019-04-11 12:15:55 -0700

    reordered

--- webapp/backend/test.py
+++
@@ -1,7 +0,0 @@
-from user_dao_impl import UserDaoImpl
-
-if __name__ == "__main__":
-    dao = UserDaoImpl()
-    # print(dao.fetch_employer_data("sGEDeVZW0BXQtYsuOmWBZWiijn83"))
-    print(dao.fetch_all_teachers())
-    
